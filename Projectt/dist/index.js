991007489e60606445f00b238a74cce539ce2d67
faizzz11 <faizmoulavi54@gmail.com>
2025-03-19 22:11:44 +0530

    class in TS also public, private and protected keywords in TS class

--- Projectt/dist/index.js
+++ Projectt/dist/index.js
@@ -1,3 +1,47 @@
 "use strict";
-console.log("TypeScript Sikh rha huu!!!!!!");
-console.log("duplicate krke dekh rha hu watch mode barabar chal rha hai ya nahi");
+// console.log("TypeScript Sikh rha huu!!!!!!")
+// console.log("duplicate krke dekh rha hu watch mode barabar chal rha hai ya nahi")
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "mira road"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "mira road";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("bsdk ek se jyada course hone chahiye");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const faizzz = new User("f@m.com", "faizzz");
+// faizzz.name 
+// faizzz.deleteToken()
--- Projectt/src/index.ts
+++ Projectt/src/index.ts
@@ -1,2 +1,56 @@
-console.log("TypeScript Sikh rha huu!!!!!!")
-console.log("duplicate krke dekh rha hu watch mode barabar chal rha hai ya nahi")
+// console.log("TypeScript Sikh rha huu!!!!!!")
+// console.log("duplicate krke dekh rha hu watch mode barabar chal rha hai ya nahi")
+
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "mira road"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+
+//     }
+// }
+class User {
+
+    protected _courseCount = 1
+    
+    readonly city: string = "mira road"
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){
+    }
+    private deleteToken(){
+        console.log("Token deleted");
+        
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("bsdk ek se jyada course hone chahiye")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const faizzz = new User("f@m.com","faizzz")
+// faizzz.name 
+
+// faizzz.deleteToken()
